fca001715f1c550f6a4107b5dab1eadc787fa16b
Oleksandr Sergyeyev <explod1983@gmail.com>
2020-07-29 21:45:16 +0200

    Fix json path

--- static_data.py
+++ static_data.py
@@ -2,8 +2,8 @@ import os
 import json
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-with open('static_data_site_info.json', 'r') as file:
+db_file_path = DIR_PATH + '/static_data_site_info.json'
+with open(db_file_path, 'r') as file:
     data_base = json.load(file)
 
 web_sites = data_base['web_sites']
